9b1860632dc1e2c13242c6549dceb237c527a18a
Daniel Cornejo <contact@danielcornejo.dev>
2023-07-29 17:38:06 -0400

    workout model update for apiExercises array

--- models/Workout.model.js
+++ models/Workout.model.js
@@ -3,7 +3,8 @@ const { Schema, model } = require("mongoose");
 const workoutSchema = new Schema(
   {
     date: Date,
-    exercises: [{type: Schema.Types.ObjectId, ref: 'Exercise'}]
+    exercises: [{type: Schema.Types.ObjectId, ref: 'Exercise'}],
+    apiExercises: [String] //pass in id's from API
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`

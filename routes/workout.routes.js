0ce5542af202e72d13c7f851e2d025f6e8908cc3
Daniel Cornejo <contact@danielcornejo.dev>
2023-08-19 13:10:37 -0400

    current code

--- routes/workout.routes.js
+++ routes/workout.routes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const Workout = require('../models/Workout.model');
 const Exercise = require('../models/Exercise.model')
-const User = require('../models/User.model')
 
 // CREATE POST
 router.post("/", (req, res) => {

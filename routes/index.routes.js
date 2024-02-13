2465f980072f54f5306d9ceb442f9b573acf16e1
Daniel Cornejo <contact@danielcornejo.dev>
2024-02-12 21:03:09 -0500

    catch error in index.routes.js

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
+}).catch((err) => {
+  console.error("An error occurred:", err);
+  next(err); // Pass the error to the next error-handling middleware
 });
 
 module.exports = router;

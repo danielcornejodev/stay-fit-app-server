583290d4c28c7e7fbc8881dbd4539281122103d3
Daniel Cornejo <contact@danielcornejo.dev>
2024-02-17 15:09:41 -0500

    cors frontend url

--- config/index.js
+++ config/index.js
@@ -26,15 +26,15 @@ module.exports = (app) => {
   app.set("trust proxy", 1);
 
   // controls a very specific header to pass headers from the frontend
-  // app.use(
-  //   cors({
-  //     origin: [FRONTEND_URL],
-  //   })
-  // );
-
   app.use(
-    cors(true)
+    cors({
+      origin: [FRONTEND_URL],
+    })
   );
+
+  // app.use(
+  //   cors(true)
+  // );
 
   // app.use(
   //   session({
